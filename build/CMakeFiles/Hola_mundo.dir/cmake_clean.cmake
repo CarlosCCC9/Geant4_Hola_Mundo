4435a60986b3212a489d21a68c1f23e68f078b06
CarlosCCC9 <71854944+CarlosCCC9@users.noreply.github.com>
2025-09-01 18:27:36 -0400

    Delete build directory

--- build/CMakeFiles/Hola_mundo.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Hola_mundo"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Hola_mundo.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/Hola_mundo.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for Hola_mundo.
-# This may be replaced when dependencies are built.
